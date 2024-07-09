5b477b2ddf6efd35848e069cdd0c43e027a08b5b
Asus <Asus@LAPTOP-VFJN96BK>
2024-07-09 19:33:14 +0530

    task1 form developer1

---
+++ Proj1Git/src/main/java/com/kalyan/main/AppTest.java
@@ -0,0 +1,16 @@
+package com.kalyan.main;
+import com.kalyan.service.ArithematicOperation;
+public class AppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("kalyan");
+ArithematicOperation op=new ArithematicOperation();
+
+System.out.println("sum="+op.sum(10,5));
+
+System.out.println("sub="+op.sub(10, 5));
+
+	}
+
+}
---
+++ Proj1Git/src/main/java/com/kalyan/service/ArithematicOperation.java
@@ -0,0 +1,11 @@
+package com.kalyan.service;
+
+public class ArithematicOperation {
+
+	public  int sum(int a ,int b) {
+		return a+b;
+	}
+	public  int sub(int a ,int b) {
+		return a-b;
+	}
+}
