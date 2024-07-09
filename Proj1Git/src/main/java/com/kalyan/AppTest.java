0d94340e0161959a29b0d56feda1261c47dfcc87
Asus <Asus@LAPTOP-VFJN96BK>
2024-07-09 18:30:05 +0530

    initial commit

---
+++ Proj1Git/src/main/java/com/kalyan/AppTest.java
@@ -0,0 +1,10 @@
+package com.kalyan;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("kalyan chandrakant magar");
+	}
+
+}
