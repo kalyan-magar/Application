fb6469702c2bc5855dfb1af3520ef09ee8415fab
Asus <Asus@LAPTOP-VFJN96BK>
2024-07-17 23:00:21 +0530

    Developer has done task branch1

--- GitProj2/src/main/java/com/kalyan/git/payment/ClassPayment.java
+++ GitProj2/src/main/java/com/kalyan/git/payment/ClassPayment.java
@@ -1,5 +1,13 @@
 package com.kalyan.git.payment;
 
 public class ClassPayment {
+public String creditCardPayment() {
+	System.out.println("payment done");
+	return "credit card payment done";
+}
 
+public String debitcardPayment() {
+	System.out.println("payment done");
+	return "debit card payment done";
+}
 }
